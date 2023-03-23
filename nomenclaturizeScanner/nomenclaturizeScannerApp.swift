194a1653677cffa5bd5f9771e837d28eee6a22d9
Kworz <romain.sapet@metalizz.com>
2023-03-23 11:27:45 +0100

    Initial Commit

---
+++ nomenclaturizeScanner/nomenclaturizeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  nomenclaturizeScannerApp.swift
+//  nomenclaturizeScanner
+//
+//  Created by Romain Sapet on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct nomenclaturizeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
